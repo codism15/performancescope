d89368b77aedd1b29e977be020e01a1ef755e274
Rui Luo <lplusplus@gmail.com>
2018-08-27 17:11:32 -0500

    .

---
+++ ScopeProfiler/PerformanceScope.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ScopeProfiler
+{
+    public class PerformanceScope
+        : IDisposable
+    {
+        static Stack<PerformanceScope> stack = new Stack<PerformanceScope>();
+
+        PerformanceScope instance;
+
+        public string Name { get; set; }
+
+        public int CallCount { get; set; }
+
+        public Stopwatch Stopwatch { get; set; }
+
+        public List<PerformanceScope> Children { get; set; }
+
+        public PerformanceScope(string name)
+        {
+            if (stack.Count == 0)
+            {
+                Name = name;
+                Stopwatch = new Stopwatch();
+                instance = this;
+            }
+            else
+            {
+                var currentCounter = stack.Peek();
+                if (currentCounter.Children == null)
+                {
+                    currentCounter.Children = new List<PerformanceScope>();
+                }
+                instance = currentCounter.Children.FirstOrDefault(c => c.Name == name);
+                if (instance == null)
+                {
+                    Name = name;
+                    Stopwatch = new Stopwatch();
+                    instance = this;
+                    currentCounter.Children.Add(instance);
+                }
+            }
+            stack.Push(instance);
+            instance.CallCount++;
+            instance.Stopwatch.Start();
+        }
+
+        public void Dispose()
+        {
+            instance.Stopwatch.Stop();
+            stack.Pop();            
+        }
+
+        public void Report(TextWriter outs)
+        {
+            Report(outs, instance);
+        }
+
+        private void Report(TextWriter outs, PerformanceScope node, string indent = "")
+        {
+            outs.WriteLine($"{indent}{node.Name}:");
+            outs.WriteLine($"{indent}  CallCount: {node.CallCount}");
+            outs.WriteLine($"{indent}  TotalSeconds: {node.Stopwatch.Elapsed.TotalSeconds:0.0}");
+            if (node.Children != null)
+            {
+                if (node.Children.Count > 1)
+                {
+                    double totalSeconds = node.Children.Aggregate(0D, (s, ps) => s + ps.Stopwatch.Elapsed.TotalSeconds);
+                    outs.WriteLine($"{indent}  ChildTotalSeconds: {totalSeconds:0.0}");
+                }
+                var childIndent = indent + "  ";
+                foreach (var child in node.Children)
+                {
+                    Report(outs, child, childIndent);
+                }
+            }
+        }
+    }
+
+}
---
+++ ScopeProfiler/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ScopeProfiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PerformanceScope rootScope = null;
+            using (rootScope = new PerformanceScope(nameof(Main)))
+            {
+                Console.WriteLine("Hello World!");
+                for (int i = 0; i < 10; i++)
+                {
+                    TestMethod1();
+                }
+
+                for (int i = 0; i < 50; i++)
+                {
+                    TestMethod2();
+                    Thread.Sleep(10);
+                }
+            }
+
+            var outs = new StringWriter();
+            rootScope.Report(outs);
+
+            Debug.WriteLine(outs.ToString());
+        }
+
+        static void TestMethod1()
+        {
+            using (new PerformanceScope(nameof(TestMethod1)))
+            {
+                Thread.Sleep(100);
+                TestMethod2();
+            }
+        }
+
+        static void TestMethod2()
+        {
+            using (new PerformanceScope("Method 2"))
+            {
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
